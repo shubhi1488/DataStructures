04913962e2caced76049587584cdf40f74425bb5
Shubhi Agarwal <shubhi1488@gmail.com>
2022-07-26 19:35:39 +0530

    Update TheStockSpanProblem.java

--- Stack/TheStockSpanProblem.java
+++ Stack/TheStockSpanProblem.java
@@ -33,4 +33,44 @@ public class SpanValue {
     }
 }
 //............................................................................................................................................................................
-//method-2-
+//method-2-Using stack
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class StockProblem {
+    static ArrayList<Integer> calculate(int arr[],int n){
+        Deque<Integer>s=new ArrayDeque<>();
+        ArrayList<Integer>ans=new ArrayList<>();
+        for (int i=0;i<n;i++){
+            while (!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
+
+            }
+            if (s.isEmpty()){
+                ans.add(i+1);
+            }
+            else {
+                int top=s.peek();
+                ans.add(i-top);
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+    static void print(ArrayList<Integer>arr){
+        for (int i=0;i<arr.size();i++){
+            System.out.println(arr.get(i)+"");
+        }
+    }
+
+    public static void main(String[] args) {
+        int price[] = { 10, 4, 5, 90, 120, 80 };
+        int n = price.length;
+
+        ArrayList<Integer> arr = calculate(price, n);
+        print(arr);
+
+    }
+}
+//...........................................................................................................................................................................
